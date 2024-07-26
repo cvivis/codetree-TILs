39e813581aae546e4f9db7a6c139bcd263948dc3
Hwang Si-Eun <hsy0754@naver.com>
2024-07-27 01:32:43 +0900

    Title: 문자열 두번 출력하기, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240727/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static StringBuilder output = new StringBuilder();
+    static int N;
+    public static void main(String[] args) throws IOException {
+        tokens = new StringTokenizer(br.readLine());
+        String str = tokens.nextToken();
+        System.out.println(str);
+        System.out.println(str);
+    }
+}
