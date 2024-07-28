dc89e4592f67a45ef2b53f487357f0b87b89310a
Hwang Si-Eun <hsy0754@naver.com>
2024-07-29 02:54:45 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 164ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240729/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static StringBuilder output = new StringBuilder();
+    static int N;
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        printStar(N);
+    }
+//    1 2 3 4 5 6 7 8 9 0 0
+    private static void printStar(int n){
+        if(n <= 0 )return;
+        for(int i = 0; i < n;i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+        printStar(n-1);
+        for(int i = 0; i < n;i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
