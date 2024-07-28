982c7f7fdef5894066c0ec7c25b845390079e7dd
Hwang Si-Eun <hsy0754@naver.com>
2024-07-29 02:28:17 +0900

    Title: 단어로 구분하기, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240729/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static StringBuilder output = new StringBuilder();
+    static int N;
+    public static void main(String[] args) throws IOException {
+        tokens = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < 10;i++){
+            System.out.println(tokens.nextToken());
+        }
+    }
+}
