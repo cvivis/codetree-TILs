ed41b29d8282e4d00d025498bebacdfe1f129f2c
Hwang Si-Eun <hsy0754@naver.com>
2024-08-12 20:23:41 +0900

    Title: 사각형 채우기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240812/사각형 채우기 3/rectangle-fill-3.java
+++ 240812/사각형 채우기 3/rectangle-fill-3.java
@@ -17,17 +17,13 @@ public class Main {
         dp[0] = 1;
         dp[1] = 2;
         dp[2] = 7;
-        // for(int i = 3; i <= N;i++){
-        //     dp[i] = (dp[i-1] * 2 + dp[i-2] * 3) % mod;
-        //     for(int j = 3; j <= i; j++){
-        //         dp[i] = (dp[i] + dp[i-j] * 2) % mod;
-        //     }
-        // }
-        for(int i = 2; i <= N; i++) {
-            dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
-            for(int j = i - 3; j >= 0; j--)
-                dp[i] = (dp[i] + dp[j] * 2) % MOD;
+        for(int i = 3; i <= N;i++){
+            dp[i] = (dp[i-1] * 2 + dp[i-2] * 3) % mod;
+            for(int j = 3; j <= i; j++){
+                dp[i] = (dp[i] + dp[i-j] * 2) % mod;
+            }
         }
+
         System.out.println(dp[N]);
     }
 }
