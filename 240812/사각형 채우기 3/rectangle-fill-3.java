5b4601e572c1eec59cff0562c06071487ff8da2f
Hwang Si-Eun <hsy0754@naver.com>
2024-08-12 19:53:54 +0900

    Title: 사각형 채우기 3, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240812/사각형 채우기 3/rectangle-fill-3.java
+++ 240812/사각형 채우기 3/rectangle-fill-3.java
@@ -14,10 +14,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         int[] dp = new int[1000];
+        dp[0] = 1;
         dp[1] = 2;
         dp[2] = 7;
         for(int i = 3; i <= N;i++){
-            dp[i] = (dp[i-1] * 2 + (dp[i-2] * 3) + 2) % mod;
+            dp[i] = (dp[i-1] * 2 + dp[i-2] * 3) % mod;
+            for(int j = 3; j <= i; j++){
+                dp[i] = (dp[i] + dp[j] * 2) % mod;
+            }
         }
         System.out.println(dp[N]);
     }
